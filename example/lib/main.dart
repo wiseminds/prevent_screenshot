ca41fc22d8d5add8fea202bcfb09d0bc67b3bed0
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2020-09-18 08:58:59 +0100

    added ios

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -56,7 +56,6 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
 class VideoApp extends StatefulWidget {
   @override
   _VideoAppState createState() => _VideoAppState();
@@ -64,10 +63,14 @@ class VideoApp extends StatefulWidget {
 
 class _VideoAppState extends State<VideoApp> {
   VideoPlayerController _controller;
+  String platformVersion;
+
+  bool screenRecordInProgress = false;
 
   @override
   void initState() {
     super.initState();
+    initPlatformState();
     _controller = VideoPlayerController.network(
         'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')
       ..initialize().then((_) {
@@ -76,32 +79,57 @@ class _VideoAppState extends State<VideoApp> {
       });
   }
 
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    // String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await PreventScreenshot.platformVersion;
+      PreventScreenshot.statusStream.listen((event) {
+        print('new event from swift $event');
+        setState(() {
+          screenRecordInProgress = event;
+        });
+      });
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Video Demo',
-      home: Scaffold(
-        body: Center(
-          child: _controller.value.initialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )
-              : Container(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+    return Stack(
+      children: [
+        Scaffold(
+          body: Center(
+            child: _controller.value.initialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: Text(platformVersion?.toString() ?? '') ??
+                        VideoPlayer(_controller),
+                  )
+                : Container(),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _controller.value.isPlaying
+                    ? _controller.pause()
+                    : _controller.play();
+              });
+            },
+            child: Icon(
+              _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+            ),
           ),
         ),
-      ),
+        if (screenRecordInProgress)
+          Container(
+            alignment: Alignment.center,
+            color: Colors.red,
+            child: Text('Screen record in progress'),
+          )
+      ],
     );
   }
 
